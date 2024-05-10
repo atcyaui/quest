72553fa7ad8db5bac87eda3e3c8255752c49d878
atcyaui <gs1juandysp@hotmail.com>
2024-05-10 07:36:14 +0700

    add more features

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-function generate(numRows) {
-  const triangle = [];
-  for (let i = 0; i < numRows; i++) {
-    const row = new Array(i + 1).fill(1);
-    for (let j = 1; j < row.length - 1; j++) {
-      row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
-    }
-    triangle.push(row);
-  }
-  return triangle;
-}
+const dropWhile = (arr, func) => {
+  while (arr.length > 0 && !func(arr[0])) arr = arr.slice(1);
+  return arr;
+};

10b2ce96d3a08c7dd5e65e8929499ef927bffe44
atcyaui <gs1juandysp@hotmail.com>
2024-05-14 19:09:48 +0700

    demo project

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-function minPathSum(grid) {
+function numIslands(grid) {
+  if (!grid || grid.length === 0) return 0;
   const m = grid.length;
   const n = grid[0].length;
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
-  dp[0][0] = grid[0][0];
-  for (let i = 1; i < m; i++) {
-    dp[i][0] = dp[i - 1][0] + grid[i][0];
-  }
-  for (let j = 1; j < n; j++) {
-    dp[0][j] = dp[0][j - 1] + grid[0][j];
-  }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+  let islands = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === "1") {
+        islands++;
+        dfs(grid, i, j);
+      }
     }
   }
-  return dp[m - 1][n - 1];
+  return islands;
+}
+function dfs(grid, i, j) {
+  const m = grid.length;
+  const n = grid[0].length;
+  if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === "0") return;
+  grid[i][j] = "0";
+  dfs(grid, i + 1, j);
+  dfs(grid, i - 1, j);
+  dfs(grid, i, j + 1);
+  dfs(grid, i, j - 1);
 }
